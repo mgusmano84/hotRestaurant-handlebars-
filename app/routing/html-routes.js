5a33592c9066c986139c30cad7d53b178317aeb8
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-07-07 15:09:41 -0400

    redo

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,7 +3,8 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 var path = require('path');
-
+var tableData 		= require('../data/table-data.js');
+var waitListData 	= require('../data/waitinglist-data.js');
 
 
 
@@ -19,9 +20,9 @@ module.exports = function(app){
 	// In each of the below cases the user is shown an HTML page of content
 	// ---------------------------------------------------------------------------
 
-	// app.get('/tables', function(req, res){
-	// 	res.sendFile(path.join(__dirname + '/../public/tables.html'));
-	// });
+	app.get('/tables', function(req, res){
+		res.render('tables', {tabdata: tableData, resdata: waitListData});
+	});
 
 	app.get('/reserve', function(req, res){
 		res.sendFile(path.join(__dirname + '/../public/reserve.html'));
--- server.js
+++ server.js
@@ -6,10 +6,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var app = express();
 
-
-
-
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 
 
@@ -19,60 +20,17 @@ var path = require('path');
 // This sets up the basic properties for our express server 
 // ==============================================================================
 
-var app = express(); // Tells node that we are creating an "express" server
+// Tells node that we are creating an "express" server
 var PORT = process.env.PORT || 8080; // Sets an initial port. We'll use this later in our listener
-
-
-
-//adding handlebars
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 
 // BodyParser makes it easy for our server to interpret data sent to it.
 // The code below is pretty standard.
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
-
-var tableArray = [
-	{
-		customerName: "Ahmed",
-		customerEmail: "afhaque89@gmail.com",
-		customerID: "afhaque89",
-		phoneNumber: "979-587-0887",
-
-	}
-
-];
-
-var waitingArray = [
-	{
-		customerName: "Saima",
-		customerEmail: "saima@gmail.com",
-		phoneNumber: "979-587-0887",
-		customerID: "saimacool"
-	}
-];
-
-
-
-var lunches = [
-  {lunch: 'Beet & Goat Cheese Salad with minestrone soup.'},
-  {lunch: 'Pizza, two double veggie burgers, fries with a big glup'}
-];
-
-app.get('/tables', function(req,res) {
-    res.render('index', tableArray[0]);
-});
-
-// app.get('/tables', function(req,res) {
-//     res.render('index', waitingArray[0]);
-// });
 
 
 
